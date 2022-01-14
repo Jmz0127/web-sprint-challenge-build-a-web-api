08e5aca97c89ffdf7f638b4b571adc2f941997de
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-14 00:40:27 -0500

    server is up and running

--- api/server.js
+++ api/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const server = express();
 
 // Configure your server here
+
+server.use(express.json());
+server.get('/', (req, res) => {
+	res.send(`<h2>Here is the start of things!</h2>`);
+});
+
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
